87529fb00057ea47638f2c3f98516f39bca84cd4
SiddharthaRoy1 <roysiddhartha1999@gmail.com>
2021-09-09 12:25:07 +0530

    Renamed Home.html to Index.html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 mongoose = require('mongoose');
 // Database URL
-const DATABASE = 'mongodb+srv://Siddhartha123:tylerdurden@cluster0.6o2da.mongodb.net/test?retryWrites=true&w=majority'
+
+const DATABASE = 'mongodb+srv://Siddhartha123:tylerdurden@cluster0.6o2da.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(DATABASE, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -26,12 +27,12 @@ app.use(express.urlencoded({ extended: true }));
 
 const Url = mongoose.model('Url');
 
-// Create Routes
+// GET request to serve index.html
 app.get('/', (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   });
-  fs.readFile('./views/home.html', null, function (error, data) {
+  fs.readFile('./views/index.html', null, function (error, data) {
     if (error) {
       res.writeHead(404);
       res.write('Route not found!');
@@ -41,19 +42,19 @@ app.get('/', (req, res) => {
     res.end();
   });
 })
-
+// GET request to redirect the shortened URL to the original URL
 app.get('/:route', async (req, res) => {
   const route = req.params.route;
   const instance = await Url.findOne({id: route});
   if(instance) {
     instance.visitors = instance.visitors + 1;
     await instance.save();
-    res.redirect(`//${instance.url}`)
+    res.redirect(instance.url)
   } else {
     res.send("404")
   }
 })
-
+// GET request to serve count.html and show analytics
 app.get('/analytics/:route', async (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html'
@@ -68,7 +69,7 @@ app.get('/analytics/:route', async (req, res) => {
     res.end();
   });
 })
-
+// POST request to send the number of visitors to count.html
 app.post('/analytics', async (req, res) => {
   const route = req.body.route;
   const instance = await Url.findOne({id: route});
@@ -77,7 +78,7 @@ app.post('/analytics', async (req, res) => {
     message: "Number of visitors sent!"
   })
 })
-
+// POST request for shortening the URL
 app.post('/', async (req, res) => {
   const url = req.body.url;
   const instance = new Url({
@@ -90,10 +91,10 @@ app.post('/', async (req, res) => {
   await instance.save()
   res.send({
     message: `${id} was created`,
-    url: `${id}`,
+    url: id,
   });
 })
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log('Listening on port 8000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port 3000');
 })
